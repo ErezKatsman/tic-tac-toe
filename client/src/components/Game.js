5137467a28e01ad859fa2f84f68ca4a333948bae
ErezKatsman <erezkatsman2311@gmail.com>
2021-08-18 15:21:21 +0300

    finihed and deployed
    1. added favicom and image
    2. responsive for phones
    3. adding some me styling

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Board from "./Board";
 import { calculateWinner } from "../utils";
+import Image from "../images/ticTac.jpg";
 
 export default function Game() {
   const [isDark, setIsDark] = useState(false);
 
   const style = isDark
-    ? { game: "game ", btn: "btn" }
-    : { game: "game game-light", btn: "btn btn-light" };
+    ? { game: "game ", btn: "btn", overlay: "overlay", text: "text" }
+    : {
+        game: "game game-light",
+        btn: "btn btn-light",
+        overlay: "overlay overlay-light",
+        text: "text text-light",
+      };
 
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
@@ -57,7 +63,7 @@ export default function Game() {
           onClick={() => setIsDark((prev) => !prev)}
           className={`corner ${style.btn}`}
         >
-          dark/light
+          {isDark ? "Light" : "Dark"}
         </button>
         <h3>
           The winner is <span className={winner}>{winner}</span>
@@ -75,7 +81,7 @@ export default function Game() {
           onClick={() => setIsDark((prev) => !prev)}
           className={`corner ${style.btn}`}
         >
-          dark/light
+          {isDark ? "Light" : "Dark"}
         </button>
         <h3>Draw! There in no winner</h3>
         <button className={style.btn} onClick={startNewGame}>
@@ -90,9 +96,19 @@ export default function Game() {
         onClick={() => setIsDark((prev) => !prev)}
         className={`corner ${style.btn}`}
       >
-        dark/light
+        {isDark ? "Light" : "Dark"}
       </button>
-      <h1>Tic Tac Toe</h1>
+      {/* <h1>Tic Tac Toe</h1> */}
+      <div className="container">
+        <img className="img" src={Image} />
+        <div className={style.overlay}>
+          <div className={style.text}>
+            <h4>Welcome Tic Tac Toe Game!</h4>
+            <br />
+            All you have to do is to take <br /> a frined and try to Defeat him
+          </div>
+        </div>
+      </div>
       <Board
         squares={history[stepNumber]}
         onClick={handleClick}
